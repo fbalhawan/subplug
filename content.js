9f175d49e3de4488ea3cae9ec0df4035911ae34b
fbalhawan <balhawan.fouad@gmail.com>
2023-07-21 21:04:53 +0300

    WIP: Append form to Youtube HTML

--- content.js
+++ content.js
@@ -1,10 +1,35 @@
 let subtitles = [];
 let subtitleDiv = null;
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+function appendForm() {
+    console.log("Called!");
+    const formHtml = `
+    <form id="myForm">
+      <label for="fname">First Name:</label><br>
+      <input type="text" id="fname" name="fname"><br>
+      <label for="lname">Last Name:</label><br>
+      <input type="text" id="lname" name="lname">
+      <input type="submit" value="Submit">
+    </form>`;
+
+    // YouTube video description container
+    const descriptionDiv = document.querySelector('#bottom-row #description');
+
+    if (descriptionDiv) {
+        const div = document.createElement('div');
+        div.innerHTML = formHtml;
+        descriptionDiv.appendChild(div);
+    } else {
+        console.log('Cannot find the description div');
+    }
+}
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "display") {
         subtitles = JSON.parse(request.subtitles);
-        
+
         // If the div doesn't exist, create it
         if (!subtitleDiv) {
             subtitleDiv = document.createElement('div');
@@ -20,7 +45,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
         // Assume the first video on the page is the one to be subtitled
         let video = document.getElementsByTagName('video')[0];
-        video.ontimeupdate = function() {
+        video.ontimeupdate = function () {
             subtitleDiv.textContent = '';
             for (let subtitle of subtitles) {
                 if (video.currentTime >= subtitle.start && video.currentTime <= subtitle.end) {
@@ -30,3 +55,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
     }
 });
+
+console.log("Calling append")
+appendForm();
--- popup.js
+++ popup.js
@@ -1,8 +1,43 @@
-document.getElementById('submit').addEventListener('click', function() {
-    let subtitles = document.getElementById('subtitles').value;
-    // video = document.getElementsByTagName('video')[0];
+let submitBtn = document.getElementById('submit');
+let openFormBtn = document.getElementById('openFormBtn');
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "display", subtitles: subtitles});
+
+if (document.readyState !== 'loading') {
+    appendForm();
+}
+else{
+    document.addEventListener("DOMContentLoaded", async () => {
+        console.log("Loaded")
+        
+        const queryParameters = activeTab.url.split("?")[1];
+        const urlParameters = new URLSearchParams(queryParameters);
+        const currentVideo = urlParameters.get("v");
+        
+        console.log("currentView: ",currentVideo);
+        
     });
-});
+}
+
+
+// Function that appends a form under a YouTube video
+function appendForm() {
+    const formHtml = `
+    <form id="myForm">
+      <label for="fname">First Name:</label><br>
+      <input type="text" id="fname" name="fname"><br>
+      <label for="lname">Last Name:</label><br>
+      <input type="text" id="lname" name="lname">
+      <input type="submit" value="Submit">
+    </form>`;
+
+    // YouTube video description container
+    const descriptionDiv = document.querySelector('#bottom-row #description');
+
+    if (descriptionDiv) {
+        const div = document.createElement('div');
+        div.innerHTML = formHtml;
+        descriptionDiv.appendChild(div);
+    } else {
+        console.log('Cannot find the description div');
+    }
+}
